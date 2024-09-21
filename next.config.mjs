96dbe7d70bab2de0c5020d039bddbf2a23104a6a
CedNet Development <31966673+cednetdev@users.noreply.github.com>
2024-09-21 09:51:19 -0300

    Atualizando projeto

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ["api.ts", "page.tsx", "api.tsx"],
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        hostname: "prv-s3.grupocednet.com.br",
+        protocol: "https",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
