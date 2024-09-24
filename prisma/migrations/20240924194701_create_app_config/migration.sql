b26e84f29ca372963d85684660679a5f193258fc
CedNet Development <31966673+cednetdev@users.noreply.github.com>
2024-09-24 16:53:35 -0300

    Criado configurações de aplicativo.

---
+++ prisma/migrations/20240924194701_create_app_config/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE `app_config` (
+    `id` VARCHAR(191) NOT NULL,
+    `value` TEXT NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `description` TEXT NOT NULL,
+    `created_at` DATETIME(3) NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    UNIQUE INDEX `app_config_name_key`(`name`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
---
+++ src/pages/api/settings/app/store.ts
@@ -0,0 +1,49 @@
+import { apiHandlerErros } from "@/exceptions/api_handler_erros";
+import { errorInvalidContentBody } from "@/exceptions/invalid_content_body";
+import { prisma } from "@/libs/prisma";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export async function store(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const { value, name, description } = req.body;
+
+    // Validando content.
+    errorInvalidContentBody({ value, name, description }, [
+      "value|É necessário informar o valor para a configuração.",
+      "name|É necessário informar um nome para a configuração.",
+      "description|Informe uma descrição para a configuração.",
+    ]);
+
+    const existsConfig = await prisma.appConfig.count({
+      where: {
+        name,
+      },
+    });
+
+    if (existsConfig >= 1) {
+      throw new Error(`O nome ${name} já está em uso para um configuração.`);
+    }
+
+    const config = await prisma.appConfig.create({
+      data: { value, name, description },
+    });
+
+    return res.status(200).json({
+      status: true,
+      message: "Configuração criado com sucesso.",
+      data: config,
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      return apiHandlerErros(error, res);
+    }
+
+    return res.status(400).json({
+      status: false,
+      message: "Erro desconhecido.",
+      data: null,
+    });
+  } finally {
+    await prisma.$disconnect();
+  }
+}
